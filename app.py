b3ba7f1ca542c6e7ac4d41282fc3b5e447072b14
M MOHAN <muneppamohan@gmail.com>
2026-02-28 19:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -246,5 +246,8 @@ def dashboard():
         health_guide=health_guide
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(threaded=True, debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
